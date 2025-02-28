06621392d22913c7996bd9c04feb0a2fda534c38
Ayelid <aarevaloq@ufpso.edu.co>
2025-02-28 13:07:53 -0500

    Ejercicio 2 de la clase

--- Ejercicio5/src/Media.java
+++ Ejercicio5/src/Media.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Media {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese el primer numero: ");
+        double numUno = scanner.nextDouble();
+
+        System.out.print("Ingrese el segundo numero: ");
+        double numDos = scanner.nextDouble();
+
+        System.out.print("Ingrese el tercer numero: ");
+        double numTres = scanner.nextDouble();
+
+        double suma = 0;
+
+        suma = numUno + numDos + numTres;
+        double media = suma / 3;
+
+        System.out.println("El resultado es: " + media);
+    }
     
 }
